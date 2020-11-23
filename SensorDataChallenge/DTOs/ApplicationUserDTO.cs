569682a6d197929361641dd3962c70f87f03cd8c
Javier Gonzalez Espejo <javiergespejo@gmail.com>
2020-11-22 22:47:26 -0300

    DTOs created

---
+++ SensorDataChallenge/DTOs/ApplicationUserDTO.cs
@@ -0,0 +1,28 @@
+﻿using SensorDataChallenge.Models;
+using System.Collections.Generic;
+
+namespace SensorDataChallenge.DTOs
+{
+    public class ApplicationUserDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public Client Client { get; set; }
+        public enum PermissionEnum
+        {
+            ViewUsers, // Permission to view users in the system
+            PostUser, // Permission to register a user in the system
+            PutUser, // Permission to modify a user in the system
+            DeleteUser, // Permission to unsubscribe a user from the system
+            ViewClients, // Permission to view clients in the system
+            PostClient, // Permission to register a client in the system
+            PutClient, // Permission to modify a client in the system
+            DeleteClient, // Permission to remove a client from the system
+            ViewMap  // Permission to view the map
+        }
+        public List<PermissionEnum> Permission { get; set; }
+    }
+}
---
+++ SensorDataChallenge/DTOs/ClientDTO.cs
@@ -0,0 +1,36 @@
+﻿namespace SensorDataChallenge.DTOs
+{
+    public class ClientDTO
+    {
+        public int Id { get; set; }
+        public string BusinessName { get; set; }
+        public int RucNum { get; set; }
+        public string Address { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public int PostalCode { get; set; }
+        public enum Zone
+        {
+            Norte,
+            Centro,
+            Sur
+        }
+        public string Phone { get; set; }
+        public string Fax { get; set; }
+        public string Email { get; set; }
+        public string Web { get; set; }
+        public enum UruguayTransit
+        {
+            Si,
+            No,
+            Opcional
+        }
+        public enum UruguayLooseCargoTransit
+        {
+            Si,
+            No,
+            Opcional
+        }
+        public bool IsActive { get; set; }
+    }
+}
