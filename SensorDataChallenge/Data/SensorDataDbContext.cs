e731a60ac9f765c85d7a18e32a992de14f3cf550
Javier Gonzalez Espejo <javiergespejo@gmail.com>
2020-11-23 17:46:01 -0300

    Add Permission table and seed

--- SensorDataChallenge/Data/SensorDataDbContext.cs
+++ SensorDataChallenge/Data/SensorDataDbContext.cs
@@ -16,5 +16,58 @@ namespace SensorDataChallenge.Data
 
         public DbSet<Client> Client { get; set; }
         public DbSet<ApplicationUser> ApplicationUser { get; set; }
+        public DbSet<Permission> Permission { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Permission>()
+                .HasData(
+                    new Permission
+                    {
+                        Id = 1,
+                        Description = "ViewUsers"
+                    },
+                    new Permission
+                    {
+                        Id = 2,
+                        Description = "CreateUser"
+                    },
+                    new Permission
+                    {
+                        Id = 3,
+                        Description = "UpdateUser"
+                    },
+                    new Permission
+                    {
+                        Id = 4,
+                        Description = "DeleteUser"
+                    },
+                    new Permission
+                    {
+                        Id = 5,
+                        Description = "ViewClients"
+                    },
+                    new Permission
+                    {
+                        Id = 6,
+                        Description = "CreateClient"
+                    },
+                    new Permission
+                    {
+                        Id = 7,
+                        Description = "UpdateClient"
+                    },
+                    new Permission
+                    {
+                        Id = 8,
+                        Description = "DeleteClient"
+                    },
+                    new Permission
+                    {
+                        Id = 9,
+                        Description = "ViewMap"
+                    }
+                );
+        }
     }
 }
