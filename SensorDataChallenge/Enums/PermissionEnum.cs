f7b3fe2eeeb5d13c18bbd85755fdce271484ddb1
Javier Gonzalez Espejo <javiergespejo@gmail.com>
2020-11-23 17:14:16 -0300

    Create PermissionEnum

---
+++ SensorDataChallenge/Enums/PermissionEnum.cs
@@ -0,0 +1,15 @@
+﻿namespace SensorDataChallenge.Enums
+{
+    public enum PermissionEnum
+    {
+        ViewUsers, // Permission to view users in the system
+        PostUser, // Permission to register a user in the system
+        PutUser, // Permission to modify a user in the system
+        DeleteUser, // Permission to unsubscribe a user from the system
+        ViewClients, // Permission to view clients in the system
+        PostClient, // Permission to register a client in the system
+        PutClient, // Permission to modify a client in the system
+        DeleteClient, // Permission to remove a client from the system
+        ViewMap  // Permission to view the map
+    }
+}
--- SensorDataChallenge/Models/ApplicationUser.cs
+++ SensorDataChallenge/Models/ApplicationUser.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using SensorDataChallenge.Enums;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace SensorDataChallenge.Models
@@ -16,18 +17,6 @@ namespace SensorDataChallenge.Models
         [DataType(DataType.Password)]
         public string Password { get; set; }
         public Client Client { get; set; }
-        public enum PermissionEnum
-        {
-            ViewUsers, // Permission to view users in the system
-            PostUser, // Permission to register a user in the system
-            PutUser, // Permission to modify a user in the system
-            DeleteUser, // Permission to unsubscribe a user from the system
-            ViewClients, // Permission to view clients in the system
-            PostClient, // Permission to register a client in the system
-            PutClient, // Permission to modify a client in the system
-            DeleteClient, // Permission to remove a client from the system
-            ViewMap  // Permission to view the map
-        }
         public List<PermissionEnum> Permission { get; set; }
     }
 }
