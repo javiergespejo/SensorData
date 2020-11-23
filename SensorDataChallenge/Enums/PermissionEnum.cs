61310332ea62f49dad28335ff86b92eefbdca6b4
Javier Gonzalez Espejo <javiergespejo@gmail.com>
2020-11-23 17:47:10 -0300

    Rename

--- SensorDataChallenge/Enums/PermissionEnum.cs
+++ SensorDataChallenge/Enums/PermissionEnum.cs
@@ -2,13 +2,13 @@
 {
     public enum PermissionEnum
     {
-        ViewUsers, // Permission to view users in the system
-        PostUser, // Permission to register a user in the system
-        PutUser, // Permission to modify a user in the system
+        ViewUsers = 1, // Permission to view users in the system
+        CreateUser, // Permission to register a user in the system
+        UpdateUser, // Permission to modify a user in the system
         DeleteUser, // Permission to unsubscribe a user from the system
         ViewClients, // Permission to view clients in the system
-        PostClient, // Permission to register a client in the system
-        PutClient, // Permission to modify a client in the system
+        CreateClient, // Permission to register a client in the system
+        UpdateClient, // Permission to modify a client in the system
         DeleteClient, // Permission to remove a client from the system
         ViewMap  // Permission to view the map
     }
