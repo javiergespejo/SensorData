3f35c0cd3411cd2daa7798797c05de749ad5e2db
Javier Gonzalez Espejo <javiergespejo@gmail.com>
2020-11-22 22:52:38 -0300

    DbContext for EFC added

---
+++ SensorDataChallenge/Data/SensorDataDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SensorDataChallenge.Models;
+
+namespace SensorDataChallenge.Data
+{
+    public class SensorDataDbContext : DbContext
+    {
+        public SensorDataDbContext()
+        {
+        }
+
+        public SensorDataDbContext(DbContextOptions<SensorDataDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Client> Client { get; set; }
+        public DbSet<ApplicationUser> ApplicationUser { get; set; }
+    }
+}
