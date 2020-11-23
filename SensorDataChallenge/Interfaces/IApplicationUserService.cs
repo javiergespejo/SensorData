124e69242dc0452d0a84bc08557d67d5d3e2e2aa
Javier Gonzalez Espejo <javiergespejo@gmail.com>
2020-11-22 22:51:50 -0300

    ApplicationUserService created

---
+++ SensorDataChallenge/Interfaces/IApplicationUserService.cs
@@ -0,0 +1,19 @@
+﻿using SensorDataChallenge.DTOs;
+using SensorDataChallenge.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SensorDataChallenge.Interfaces
+{
+    public interface IApplicationUserService
+    {
+        public Task<IEnumerable<ApplicationUserDTO>> GetAllUsers();
+        public ApplicationUser EntityToEntityDTO(ApplicationUserDTO userDto);
+        public ApplicationUserDTO EntityDTOToEntity(ApplicationUser user);
+        public Task<ApplicationUser> GetUserById(int id);
+        public Task<bool> UserExist(ApplicationUser user);
+        public void AddAndSave(ApplicationUser user);
+        public void UpdateAndSave(ApplicationUser user);
+        public void DeleteAndSave(int id);
+    }
+}
---
+++ SensorDataChallenge/Services/ApplicationUserService.cs
@@ -0,0 +1,69 @@
+﻿using AutoMapper;
+using SensorDataChallenge.DTOs;
+using SensorDataChallenge.Interfaces;
+using SensorDataChallenge.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SensorDataChallenge.Services
+{
+    public class ApplicationUserService : IApplicationUserService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public ApplicationUserService(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+        public async Task<IEnumerable<ApplicationUserDTO>> GetAllUsers()
+        {
+            var users = await _unitOfWork.ApplicationUserRepository.GetAllAsync();
+            var usersDto = _mapper.Map<List<ApplicationUserDTO>>(users);
+            return usersDto;
+        }
+
+        public ApplicationUser EntityToEntityDTO(ApplicationUserDTO userDto)
+        {
+            var user = _mapper.Map<ApplicationUser>(userDto);
+            return user;
+        }
+
+        public ApplicationUserDTO EntityDTOToEntity(ApplicationUser user)
+        {
+            var userDto = _mapper.Map<ApplicationUserDTO>(user);
+            return userDto;
+        }
+
+        public async Task<ApplicationUser> GetUserById(int id)
+        {
+            var user = await _unitOfWork.ApplicationUserRepository.GetByIdAsync(id);
+            return user;
+        }
+
+        public async Task<bool> UserExist(ApplicationUser user)
+        {
+            var userExist = await _unitOfWork.ApplicationUserRepository.UserExist(user);
+            return userExist;
+        }
+
+        public void AddAndSave(ApplicationUser user)
+        {
+            _unitOfWork.ApplicationUserRepository.Add(user);
+            _unitOfWork.SaveChangesAsync();
+        }
+
+        public void UpdateAndSave(ApplicationUser user)
+        {
+            _unitOfWork.ApplicationUserRepository.Update(user);
+            _unitOfWork.SaveChangesAsync();
+        }
+
+        public void DeleteAndSave(int id)
+        {
+            _unitOfWork.ApplicationUserRepository.DeleteAsync(id);
+            _unitOfWork.SaveChangesAsync();
+        }
+    }
+}
