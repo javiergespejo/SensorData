83975532608ecc6c4c96db42af8d52df9aea230e
Javier Gonzalez Espejo <javiergespejo@gmail.com>
2020-11-23 17:46:30 -0300

    Add Permission entity

---
+++ SensorDataChallenge/Models/Permission.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace SensorDataChallenge.Models
+{
+    public class Permission : BaseEntity
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Description { get; set; }
+        public IEnumerable<ApplicationUser> Users { get; set; }
+    }
+}
