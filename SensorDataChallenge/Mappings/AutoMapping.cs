1be8763ab27ae600c47fb23e3e9ffd1b46ce3c48
Javier Gonzalez Espejo <javiergespejo@gmail.com>
2020-11-22 22:47:52 -0300

    AutoMapper class created

---
+++ SensorDataChallenge/Mappings/AutoMapping.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using SensorDataChallenge.DTOs;
+using SensorDataChallenge.Models;
+
+namespace SensorDataChallenge.Mappings
+{
+    public class AutoMapping: Profile
+    {
+        public AutoMapping()
+        {
+            //CreateMap<Entity, EntityDTO>().ReverseMap();
+            CreateMap<Client, ClientDTO>().ReverseMap();
+            CreateMap<ApplicationUser, ApplicationUserDTO>().ReverseMap();
+        }
+    }
+}
