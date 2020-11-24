a2ff6721a5df3e61507c0f7d2befa748c7bb5c1e
Javier Gonzalez Espejo <javiergespejo@gmail.com>
2020-11-24 09:40:57 -0300

    Fix ApplicationUserDTO

--- SensorDataChallenge/DTOs/ApplicationUserDTO.cs
+++ SensorDataChallenge/DTOs/ApplicationUserDTO.cs
@@ -11,18 +11,6 @@ namespace SensorDataChallenge.DTOs
         public string UserName { get; set; }
         public string Password { get; set; }
         public Client Client { get; set; }
-        public enum PermissionEnum
-        {
-            ViewUsers, // Permission to view users in the system
-            PostUser, // Permission to register a user in the system
-            PutUser, // Permission to modify a user in the system
-            DeleteUser, // Permission to unsubscribe a user from the system
-            ViewClients, // Permission to view clients in the system
-            PostClient, // Permission to register a client in the system
-            PutClient, // Permission to modify a client in the system
-            DeleteClient, // Permission to remove a client from the system
-            ViewMap  // Permission to view the map
-        }
-        public List<PermissionEnum> Permission { get; set; }
+        public List<Permission> Permission { get; set; }
     }
 }
