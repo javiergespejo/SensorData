ce4705243912fd93dc91ce23e05c5a117bd5daef
Javier Gonzalez Espejo <javiergespejo@gmail.com>
2020-11-24 09:40:30 -0300

    Create ApplicationUserPublicViewDTO

---
+++ SensorDataChallenge/DTOs/ApplicationUserPublicViewDTO.cs
@@ -0,0 +1,19 @@
+﻿using SensorDataChallenge.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SensorDataChallenge.DTOs
+{
+    public class ApplicationUserPublicViewDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [Display(Name = "Username")]
+        public string UserName { get; set; }
+        public Client Client { get; set; }
+    }
+}
