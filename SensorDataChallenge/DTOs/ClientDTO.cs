ea3e0d24b3b624d5c32b9ae5f41bc10827bc4d9b
Javier Gonzalez Espejo <javiergespejo@gmail.com>
2020-11-24 17:24:18 -0300

    Add DataAnnotations

--- SensorDataChallenge/DTOs/ClientDTO.cs
+++ SensorDataChallenge/DTOs/ClientDTO.cs
@@ -1,13 +1,24 @@
-﻿namespace SensorDataChallenge.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SensorDataChallenge.DTOs
 {
     public class ClientDTO
     {
         public int Id { get; set; }
+        [Required]
+        [Display(Name = "Business Name")]
         public string BusinessName { get; set; }
+        [Required]
+        [Display(Name = "RUC Number")]
         public int RucNum { get; set; }
+        [Required]
         public string Address { get; set; }
+        [Required]
         public string Country { get; set; }
+        [Required]
         public string City { get; set; }
+        [Display(Name = "Postal Code")]
+        [DataType(DataType.PostalCode)]
         public int PostalCode { get; set; }
         public enum Zone
         {
@@ -15,8 +26,11 @@
             Centro,
             Sur
         }
+        [DataType(DataType.PhoneNumber)]
         public string Phone { get; set; }
         public string Fax { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
         public string Web { get; set; }
         public enum UruguayTransit
