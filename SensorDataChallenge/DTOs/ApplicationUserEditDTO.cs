6e8959ab039148a663ec279971c793352344db2f
Javier Gonzalez Espejo <javiergespejo@gmail.com>
2020-11-24 15:53:09 -0300

    Create ApplicationUserEditDTO

---
+++ SensorDataChallenge/DTOs/ApplicationUserEditDTO.cs
@@ -0,0 +1,16 @@
+﻿using SensorDataChallenge.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SensorDataChallenge.DTOs
+{
+    public class ApplicationUserEditDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Permission> Permission { get; set; }
+    }
+}
