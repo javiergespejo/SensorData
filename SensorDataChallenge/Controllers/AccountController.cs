6e33582c4786b88d0b7f1bd81bc1e23afa7b240e
Javier Gonzalez Espejo <javiergespejo@gmail.com>
2020-11-25 20:18:16 -0300

    Add Login
    Controller, views and DTOs

--- SensorDataChallenge/Controllers/AccountController.cs
+++ SensorDataChallenge/Controllers/AccountController.cs
@@ -8,56 +8,97 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
 
 namespace SensorDataChallenge.Controllers
 {
     public class AccountController : Controller
     {
-        public AccountController()
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager,
+            SignInManager<IdentityUser> signInManager)
         {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
         }
 
+        // GET: Account/Register
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        // POST: Account/Register
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterDTO model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Copy data from RegisterViewModel to IdentityUser
+                var user = new IdentityUser
+                {
+                    UserName = model.UserName,
+                };
+
+                // Store user data in AspNetUsers database table
+                var result = await userManager.CreateAsync(user, model.Password);
+
+                // If user is successfully created, sign-in the user using
+                // SignInManager and redirect to index action of HomeController
+                if (result.Succeeded)
+                {
+                    await signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Index", "Home");
+                }
+
+                // If there are any errors, add them to the ModelState object
+                // which will be displayed by the validation summary tag helper
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
         public IActionResult Login()
         {
             return View();
         }
 
+        [AllowAnonymous]
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Login(string username, string password)
+        public async Task<IActionResult> Login(LoginDTO model)
         {
-            if (!string.IsNullOrEmpty(username) && string.IsNullOrEmpty(password))
+            if (ModelState.IsValid)
             {
-                return RedirectToAction("Login");
+                var result = await signInManager.PasswordSignInAsync(
+                    model.UserName, model.Password, model.RememberMe, false);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+
+                ModelState.AddModelError(string.Empty, "Invalid Login Attempt");
             }
 
-            ClaimsIdentity identity = null;
-            bool isAuthenticate = false;
-            if (username == "admin" && password == "a")
-            {
-                identity = new ClaimsIdentity(new[]
-                {
-                    new Claim(ClaimTypes.Name,username),
-                    new Claim(ClaimTypes.Role,"Admin")
-                }, CookieAuthenticationDefaults.AuthenticationScheme);
-                isAuthenticate = true;
-            }
-            if (username == "demo" && password == "c")
-            {
-                identity = new ClaimsIdentity(new[]
-                {
-                    new Claim(ClaimTypes.Name,username),
-                    new Claim(ClaimTypes.Role,"User")
-                }, CookieAuthenticationDefaults.AuthenticationScheme);
-                isAuthenticate = true;
-            }
-            if (isAuthenticate)
-            {
-                var principal = new ClaimsPrincipal(identity);
-                var login = HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
-                return RedirectToAction("Index", "Home");
-            }
-            return View();
+            return View(model);
         }
     }
 }
---
+++ SensorDataChallenge/DTOs/RegisterDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SensorDataChallenge.DTOs
+{
+    public class RegisterDTO
+    {
+        //[Required]
+        //public string Name { get; set; }
+        //[Required]
+        //public string Description { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "Password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
