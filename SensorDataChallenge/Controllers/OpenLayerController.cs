2b94e6f1f6dd78267f83bc657158317ac7a1581a
Javier Gonzalez Espejo <javiergespejo@gmail.com>
2020-11-26 22:34:45 -0300

    Remove and sort usings

--- SensorDataChallenge/Controllers/OpenLayerController.cs
+++ SensorDataChallenge/Controllers/OpenLayerController.cs
@@ -1,11 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using SensorDataChallenge.Enums;
+using SensorDataChallenge.Filters;
 
 namespace SensorDataChallenge.Controllers
 {
+    [Authorize(PermissionEnum.UpdateClient)]
     public class OpenLayerController : Controller
     {
         public IActionResult Index()
