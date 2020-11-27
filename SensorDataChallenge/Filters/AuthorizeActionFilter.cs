01caa8437b5ed0762665653d9077a734356631f8
Javier Gonzalez Espejo <javiergespejo@gmail.com>
2020-11-26 22:33:24 -0300

    Add custom Authorize

---
+++ SensorDataChallenge/Filters/AuthorizeActionFilter.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using SensorDataChallenge.Data;
+using SensorDataChallenge.Enums;
+using System.Linq;
+using System.Security.Claims;
+
+namespace SensorDataChallenge.Filters
+{
+    public class AuthorizeActionFilter : IAuthorizationFilter
+    {
+        private readonly SensorDataDbContext _context;
+        private readonly PermissionEnum _item;
+
+        public AuthorizeActionFilter(SensorDataDbContext context, PermissionEnum item)
+        {
+            _context = context;
+            _item = item;
+        }
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            bool isAuthorized = IsAuthorized(context.HttpContext.User, _item);
+
+            if (!isAuthorized)
+            {
+                context.Result = new RedirectToActionResult("Index", "Home", null);
+            }
+        }
+
+        private bool IsAuthorized(ClaimsPrincipal user, PermissionEnum item)
+        {
+            var userId = user.FindFirstValue(ClaimTypes.NameIdentifier);
+            return _context.ApplicationUser.Any(x => x.Id == userId && x.Permission.Any(x => x.Id == (int)item));
+        }
+    }
+}
---
+++ SensorDataChallenge/Filters/AuthorizeAttribute.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SensorDataChallenge.Enums;
+
+namespace SensorDataChallenge.Filters
+{
+    public class AuthorizeAttribute : TypeFilterAttribute
+    {
+        public AuthorizeAttribute(PermissionEnum item)
+        : base(typeof(AuthorizeActionFilter))
+        {
+            Arguments = new object[] { item };
+        }
+    }
+}
